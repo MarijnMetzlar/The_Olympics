af5f081503bd03419439e994762e5551585dfb4e
MarijnMetzlar <marijn_jacob@hotmail.com>
2015-10-07 14:42:52 +0200

    Made bottlecap prefab

---
+++ The Olympics/Assets/Scripts/ScriptsForOlympics/Collectable/BottlecapScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottlecapScript : MonoBehaviour 
+{
+	public float speedy = 100.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (0.0f, 0.1f, 0.0f);
+		transform.Rotate (Vector3.up, speedy * Time.deltaTime);
+		transform.Rotate (Vector3.one, speedy * Time.deltaTime);
+	}
+}
---
+++ The Olympics/Assets/Scripts/ScriptsForOlympics/Collectable/SpawnBottleCap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBottleCap : MonoBehaviour {
+
+	public Transform prefabBottleCap;
+	public float timeOfBottleCap = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timeOfBottleCap = Time.deltaTime;
+
+		if (timeOfBottleCap > 5.0f) 
+		{
+			timeOfBottleCap = 0.0f;
+			Instantiate (prefabBottleCap, new Vector3 (9.0f, 2.0f, 24.5f), Quaternion.identity);
+		}
+
+		Destroy (gameObject, 5)
+	}
+}
